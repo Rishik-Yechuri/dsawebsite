89a7b329373b9fafc591e9bdc8fc64a7c131d609
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2022-03-14 01:00:52 -0500

    Added Dropdown

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
 import './navbarcss.css';
+import dropDown from './dropdown.png';
 const navbar= () =>{
     return (
         <div className={"topBar"}>
             <div className={"halfDiv"} id={"topDiv"}>
-
+                <div className="dropdown">
+                <div  id={"dropdownDiv"}>
+                    <text id={"dropdownText"}>Structure:Maze</text>
+                    <img src={dropDown} id={"dropdownImg"}/>
+                </div>
+                <div className="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+                </div>
             </div>
             <hr className={"divider"}></hr>
             <div className={"halfDiv"} id={"bottomDiv"}>
