9bac758b4a63363bd8abc8e962d57228592d18a8
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2022-04-17 23:49:25 -0500

    Added ability to add walls

--- src/MazeComponent.js
+++ src/MazeComponent.js
@@ -1,8 +1,21 @@
 import React, {useEffect, useRef, useState} from 'react';
 import "./mazeComponentStyle.css"
 import Draggable from "react-draggable";
+import startingarrow from "./startingarrow.png";
+import targetnode from "./targetnode.png"
 
 const MazeComponent = ({state}) => {
+    var mouseDown = 0;
+    window.addEventListener('mousedown', (event) => {
+        if(event.button === 2){
+            mouseDown = 1;
+        }
+    });
+    window.addEventListener('mouseup', (event) => {
+        if(event.button === 2){
+            mouseDown = 0;
+        }
+    });
     var arr = new Array(4); // create an empty array of length n
     var holdPopups = document.getElementsByClassName("popUpContent");
     document.addEventListener('readystatechange', event => {
@@ -24,29 +37,41 @@ const MazeComponent = ({state}) => {
     useEffect(() => {
         createMaze(state);
     }, [state]);
-    function setPointValue(yCoord,xCoord,newVal){
+
+    function setPointValue(yCoord, xCoord, newVal) {
         arr[yCoord][xCoord] = newVal;
         var idName = "innerCell" + yCoord + "X" + xCoord;
+        var imgId = "innerCell" + yCoord + "X" + xCoord + "I";
         var innerCell = document.getElementById(idName);
-        if(newVal === "WALL"){
-            innerCell.style.backgroundColor = "#FFFFFF";
-        }else if(newVal === "END"){
-
-        }else if(newVal === "START"){
-            //innerCell.style.backgroundColor = "#FFFFFF";
-        }else if(newVal === "WALL"){
-            innerCell.style.backgroundColor = "#FFFFFF";
-        }else if(newVal === "PATH"){
-            innerCell.style.backgroundColor = "#FFFFFF";
-        }else if(newVal === "FINALPATH"){
-            innerCell.style.backgroundColor = "#FFFFFF";
+        var innerImg = document.getElementById(imgId);
+        arr[yCoord][xCoord] = newVal;
+        if (newVal === "END") {
+            innerCell.style.backgroundColor = "red";
+            innerImg.src = targetnode;
+            innerImg.style.display = 'block';
+        } else if (newVal === "START") {
+            innerCell.style.backgroundColor = "red";
+            innerImg.src = startingarrow;
+            innerImg.style.display = 'block';
+        } else if (newVal === "WALL") {
+            innerCell.style.backgroundColor = "#000000";
+        } else if (newVal === "PATH") {
+            innerCell.style.backgroundColor = "yellow";
+        } else if (newVal === "FINALPATH") {
+            innerCell.style.backgroundColor = "lawngreen";
+        }else{
+            innerCell.style.backgroundColor = "red";
+            innerImg.style.display = 'none';
         }
+    }
+    function getPointValue(yCoord,xCoord){
+        return arr[yCoord][xCoord];
     }
     function popupLoaded() {
         if (localStorage.getItem("popupclosed") !== "yes") {
             document.getElementById("initialPopup").style.display = "block";
         }
-        setPointValue(0,0,"WALL");
+        setPointValue(0, 0, "FINALPATH");
     }
 
     function closeButtonClicked() {
@@ -55,7 +80,10 @@ const MazeComponent = ({state}) => {
         }
         localStorage.setItem("popupclosed", "yes");
     }
-
+    function changeTile(ev){
+        var holdLoc = ev.target.id.split('innerCell')[1].split('X');
+        setPointValue(holdLoc[0],holdLoc[1],"WALL");
+    }
     function createMaze(length) {
         gridSize = length;
         arr = new Array(length);
@@ -72,19 +100,43 @@ const MazeComponent = ({state}) => {
             element.className = "wideDiv";
             for (var x = 0; x < length; x++) {
                 var insideElement = document.createElement("div");
-                insideElement.id = "innerCell" + y + "X"+ x;
+                insideElement.id = "innerCell" + y + "X" + x;
                 insideElement.className = "innerCell";
+                insideElement.onmouseenter  = (ev) => {
+                  if(mouseDown === 1){
+                      changeTile(ev)
+                      /*var holdLoc = ev.target.id.split('innerCell')[1].split('X');
+                      setPointValue(holdLoc[0],holdLoc[1],"FINALPATH");
+                      //alert("DOWN");*/
+                  }
+                }
+                insideElement.onmousedown = e =>{
+                    if(e.button === 2){
+                        changeTile(e);
+                    }
+                }
+                var insideImg = document.createElement("img");
+                insideImg.style.width = '80%';
+                insideImg.style.position = 'center';
+                insideImg.id = "innerCell" + y + "X" + x + "I";
+                insideImg.style.display = 'none';
+                //insideImg.style.height = '100%';
+                insideImg.src = startingarrow;
+                insideElement.appendChild(insideImg);
                 element.appendChild(insideElement);
             }
             document.getElementById("mazeHolderDiv").appendChild(element);
         }
     }
-
+    var rejectModernity = (event) => {
+        event.preventDefault();
+        return false;
+    }
     return (
         <div id={"mazeTopDiv"} onWheel={zoom}>
-            <Draggable>
-                <div id={"wrapperDiv"}>
-                    <div id={"mazeHolderDiv"}>
+            <Draggable oncontextmenu={rejectModernity}>
+                <div id={"wrapperDiv"} onContextMenu={rejectModernity}>
+                    <div id={"mazeHolderDiv"} onContextMenu={rejectModernity}>
                     </div>
                 </div>
             </Draggable>
